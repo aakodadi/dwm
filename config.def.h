4fa8c700d6f16a9a0692110a67c266782a498bda
AKODADI Abdelhakim <6h8wxptv@akodadi.com>
2022-05-26 21:27:49 +0200

    Change default font size of st

--- config.def.h
+++ config.def.h
@@ -58,7 +58,11 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+
+static const char *termcmd[]  = { "st",
+/* Only works if defaultfontsize patch is applied to st */
+"-z", "22",
+ NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
